66f679424b280c7a347b6ada0248b19c4dc1870e
sunf16 <sunf16@gene.com>
2023-03-20 13:37:52 -0700

    initial commit

---
+++ 2015/day1.R
@@ -0,0 +1,28 @@
+# --------------------------------------------------
+# Feiyi's trial: don't know what functions to use
+# Part 1: Count the number of opening parenthesis (a) and closing parenthesis (b). The floor number is equal to a-b
+# Part 2: each time we run sum(), test if the output equals -1?
+
+# Summary
+# Part1: 用readLine() to read text lines, strsplit(), 了解dictionary data structure (结合key和sum()做运算)
+# Part2: 看累加的结果何时为-1，累加用cumsum()
+# first character联想累加所得结果的minimum
+
+# Solution --------------------------------------------------
+# https://emilhvitfeldt.github.io/rstats-adventofcode/2015.html
+
+# Part 1
+input <- readLines("Documents/AoC/2015/data/day1_data.txt") # readLine: read text lines from a connection
+chars <- strsplit(x = input, split = "")[[1]] 
+# Split the elements of a character vector x into substrings 
+# according to the matches to substring split within them.
+
+key <- c("(" = 1, ")" = -1) 
+# A dictionary has a set of keys and each key has a single associated value.
+
+sum(key[chars]) # 280
+
+# Part 2
+min(which(cumsum(key[chars]) < 0)) # 1797 or 
+min(which(cumsum(key[chars]) == -1)) # 1797
+
